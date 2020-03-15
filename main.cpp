2bf850924973daeb43b25db5ae9ccdf34f623f41
danydacosta <danydc@comac.ch>
2020-03-15 22:26:57 +0100

    trucs bien

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/* ---------------------------
+Laboratoire : 02
+Fichier     : main.cpp
+Auteur(s)   : Valon Axel, Oliveira da Costa Dany, Allemano Enzo
+Date        : 16.03.2020
+
+But         : Ce programme permet de trouver toutes les solutions du puzzle
+ décrit sur la page suivante : https://asd1-heigvd.github.io/ASD1-Labs/puzzle/
+
+Remarque(s) :
+--------------------------- */
+
 #include "pieces.h"
 #include <iostream>
 #include <algorithm>
@@ -8,20 +20,33 @@ enum Position {
    HAUT, DROITE, BAS, GAUCHE
 };
 
-// TODO: Le prof m'a montré ça mais je ne sais pas l'utiliser, qui peut regarder ?
-/*struct parametres {
-   int largeur = 3;
-   int hauteur = 3;
-   int nombreDePiece = largeur * hauteur;
-   int nbCoteParPiece = 4;
-};*/
+struct parametres {
+   static const unsigned TAILLE_PUZZLE = 3;
+   static const unsigned NB_PIECES = TAILLE_PUZZLE * TAILLE_PUZZLE;
+   static const unsigned NB_COTES_PAR_PIECE = 4;
+};
 
+/**
+ * Fonction "maitre" appelant la fonction récursive "esclave" permettant de
+ * résoudre le puzzle
+ */
 void resoudrePuzzle(Pieces& puzzle);
 
+/**
+ * Surcharge de l'opérateur de flux permettant l'affichage des pièces des puzzles
+ * une après les autres séparé par un espace
+ */
 ostream& operator<<(ostream& lhs, const Pieces& rhs);
 
-void resoudrePuzzleRecur(Pieces& puzzle, size_t niveau);
+/**
+ * Fonction "esclave" réscursive
+ */
+void resoudrePuzzleRecur(Pieces& puzzle, unsigned niveau);
 
+/**
+ * Retourne si oui ou non deux figures sont compatibles (voir critères de
+ * compatibilité dans la donnée du labo
+ */
 bool fit(AttachementType figure1, AttachementType figure2);
 
 int main() {
@@ -33,12 +58,12 @@ int main() {
 
 ostream& operator<<(ostream& lhs, const Pieces& rhs) {
    string sortie;
-   for (size_t i = 0; i < rhs.size(); i++) {
+   for (unsigned i = 0; i < rhs.size(); i++) {
       if (i)
          sortie += " ";
-      for (size_t j = 0; j < PIECES.size(); j++) {
+      for (unsigned j = 0; j < PIECES.size(); j++) {
          Piece tmp = PIECES[j];
-         for (char k = 'a'; k <= 'd'; ++k) {
+         for (char k = 'a'; k <= 'a' + parametres::NB_COTES_PAR_PIECE; ++k) {
             if (rhs[i] == tmp) {
                sortie += to_string(j + 1) + k;
                break;
@@ -52,28 +77,29 @@ ostream& operator<<(ostream& lhs, const Pieces& rhs) {
 }
 
 void resoudrePuzzle(Pieces& puzzle) {
-   const size_t prochainNiveau = puzzle.size() - 1;
+   const unsigned PROCHAIN_NIVEAU = puzzle.size() - 1;
    const unsigned NB_ROTATION = 3; 
    for (Piece& piece : puzzle) {
-      resoudrePuzzleRecur(puzzle, prochainNiveau);
+      resoudrePuzzleRecur(puzzle, PROCHAIN_NIVEAU);
       for (unsigned j = 0; j < NB_ROTATION; j++) {
          rotate(puzzle.back().begin(), puzzle.back().begin() + 1, 
                  puzzle.back().end());
-         resoudrePuzzleRecur(puzzle, prochainNiveau);
+         resoudrePuzzleRecur(puzzle, PROCHAIN_NIVEAU);
       }
-      swap(piece, puzzle[prochainNiveau]);
+      swap(piece, puzzle[PROCHAIN_NIVEAU]);
    }
 }
 
-void resoudrePuzzleRecur(Pieces& puzzle, size_t niveau) {
-   if (niveau && niveau < 9) {
+void resoudrePuzzleRecur(Pieces& puzzle, unsigned niveau) {
+   if (niveau && niveau < parametres::NB_PIECES) {
       bool pieceOK;
-      for (size_t i = 0; i < niveau; i++) {
+      for (unsigned i = 0; i < niveau; i++) {
          pieceOK = false;
-         if (niveau % 3 && niveau <= 6) {
-            for (unsigned j = 0; j < 4; j++) {
-               if (fit(puzzle[i][(DROITE + j) % 4], puzzle[niveau][GAUCHE]) &&
-                       fit(puzzle[i][(BAS + j) % 4], puzzle[niveau + 2][HAUT])) {
+         if (niveau % parametres::TAILLE_PUZZLE && niveau <=
+         parametres::NB_PIECES - parametres::TAILLE_PUZZLE) {
+            for (unsigned j = 0; j < parametres::NB_COTES_PAR_PIECE; j++) {
+               if (fit(puzzle[i][(DROITE + j) % parametres::NB_COTES_PAR_PIECE], puzzle[niveau][GAUCHE]) &&
+                       fit(puzzle[i][(BAS + j) % parametres::NB_COTES_PAR_PIECE], puzzle[niveau + 2][HAUT])) {
                   if (j)
                      rotate(puzzle[i].begin(), puzzle[i].begin() + j, 
                              puzzle[i].end());
@@ -82,9 +108,9 @@ void resoudrePuzzleRecur(Pieces& puzzle, size_t niveau) {
                }
             }
 
-         } else if (niveau % 3) {
-            for (unsigned j = 0; j < 4; j++) {
-               if (fit(puzzle[i][(DROITE + j) % 4], puzzle[niveau][GAUCHE])) {
+         } else if (niveau % parametres::TAILLE_PUZZLE) {
+            for (unsigned j = 0; j < parametres::NB_COTES_PAR_PIECE; j++) {
+               if (fit(puzzle[i][(DROITE + j) % parametres::NB_COTES_PAR_PIECE], puzzle[niveau][GAUCHE])) {
                   if (j)
                      rotate(puzzle[i].begin(), puzzle[i].begin() + j,
                           puzzle[i].end());
@@ -93,8 +119,8 @@ void resoudrePuzzleRecur(Pieces& puzzle, size_t niveau) {
                }
             }
          } else {
-            for (unsigned j = 0; j < 4; j++) {
-               if (fit(puzzle[i][(BAS + j) % 4], puzzle[niveau + 2][HAUT])) {
+            for (unsigned j = 0; j < parametres::NB_COTES_PAR_PIECE; j++) {
+               if (fit(puzzle[i][(BAS + j) % parametres::NB_COTES_PAR_PIECE], puzzle[niveau + 2][HAUT])) {
                   if (j)
                      rotate(puzzle[i].begin(), puzzle[i].begin() + j,
                           puzzle[i].end());
